3b5680848155a3a8e3282becf5165c6fe8e1a1ca
juansvs <juans.vs@gmail.com>
2025-10-08 17:58:58 +0200

    created output folder, changed name of outfile to reflect this

--- run_FoxIBM_parallel.R
+++ run_FoxIBM_parallel.R
@@ -23,8 +23,8 @@ cl <- makeCluster(nWorkers)
 registerDoParallel(cl)
 
 #--- run code in parallel
-outfnames <- paste0("FoxSimout_", format(Sys.time(),"%y%m%d_%H%M-"),1:nWorkers,".out")
-out <- foreach(i=1:(nWorkers)) %dopar%  run_model(seed = NULL, tstep = 20, outf = outfnames[[i]], pars, S)
+outfnames <- paste0("outputs/FoxSimout_", format(Sys.time(),"%y%m%d_%H%M-"),1:nWorkers,".out")
+out <- foreach(i=1:(nWorkers)) %dopar%  run_model(seed = 234, tstep = 60, outf = outfnames[[i]], pars, S)
 
 outname <- paste0("FoxSimOut_", format(Sys.time(), format = "%y%m%d_%H%M"), ".rds")
 saveRDS(out, file = outname)
