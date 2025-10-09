64f3a8cf7f2458b8dd73d66267ed12601ae1a435
juansvs <juans.vs@gmail.com>
2025-10-09 15:17:59 +0200

    including stomach content in output

--- Fox2013_IBM.R
+++ Fox2013_IBM.R
@@ -17,8 +17,8 @@ run_model <- function(seed = NULL, tstep = 30, outf, pars, S) {
 
   # Set up output dataframe
   current_time <- 0
-  time_series <- data.frame(time = numeric(pars$total_time%/%tstep+1), avg_height = 0, avg_a = 0, avg_A = 0, sd_A = 0, avg_l = 0, avg_L = 0)
-  time_series[1,] <- c(current_time, mean(S$h), mean(S$a), mean(S$A), sd(S$A), mean(S$l), mean(S$L))
+  time_series <- data.frame(time = numeric(pars$total_time%/%tstep+1), avg_height = 0, avg_a = 0, avg_A = 0, sd_A = 0, avg_l = 0, avg_L = 0, avg_s = 0)
+  time_series[1,] <- c(current_time, mean(S$h), mean(S$a), mean(S$A), sd(S$A), mean(S$l), mean(S$L), mean(S$s))
   ix <- 2
   cat(c("time","sim_time", "avg_h", "avg_a", "avg_A", "sd_A", "avg_l", "avg_L","avg_s", "\n"), sep = "\t", file = outf)
   cat(c(0,current_time, mean(S$h), mean(S$a), mean(S$A), sd(S$A), mean(S$l), mean(S$L), mean(S$s)),"\n", sep = "\t", file = outf, append = T)
